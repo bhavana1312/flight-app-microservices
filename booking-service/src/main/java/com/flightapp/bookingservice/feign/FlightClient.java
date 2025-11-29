9b4cf674c49b1b14e4765716300c2afd09ee17df
bhavana1312 <bhavana13sree@gmail.com>
2025-11-29 22:24:51 +0530

    added feign to booking-service

---
+++ booking-service/src/main/java/com/flightapp/bookingservice/feign/FlightClient.java
@@ -0,0 +1,14 @@
+package com.flightapp.bookingservice.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name="flight-service")
+public interface FlightClient {
+
+    @PutMapping("/api/flight/update-seats/{flightId}/{count}")
+    String updateSeats(@PathVariable("flightId") Long flightId, @PathVariable("count") Integer count);
+
+    @GetMapping("/api/flight/get/{flightId}")
+    Object getFlight(@PathVariable("flightId") Long flightId);
+}
